ce97509c8b5ae8f1ab677de0229386ef74eb9690
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2022-12-07 10:03:19 -0600

    Inicializando proyecto Mostrando gatos

---
+++ src/main/java/com/mycompany/catapi/CatApi.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.catapi;
+
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Noena
+ */
+public class CatApi {
+
+    public static void main(String[] args) throws IOException {
+        GatosService catService = new GatosService();
+        catService.getGatos();
+        
+        int opcionMenu = -1;
+            String[] opciones = {
+                "1. Ver gatos",
+                "2. Salir"
+            };
+            do{
+                String opcion = (String)JOptionPane.showInputDialog(null, "Gatito java","Menú Principal", 
+                        JOptionPane.INFORMATION_MESSAGE,null,opciones,opciones[0]);
+                
+                for(int i = 0; i < opciones.length; i++){
+                    if(opcion.equals(opciones[i])){
+                        opcionMenu = i;
+                    }
+                    GatosService service = new GatosService();
+                    switch(opcionMenu){
+                        case 0:
+                            System.out.println("Vas a ver un gato");
+                            service.getGatos();
+                            break;
+                        case 1:
+                            System.out.println("Vas a salir del sistema");
+                            
+                    }
+                }
+            }while(opcionMenu != 1);
+    }
+}
